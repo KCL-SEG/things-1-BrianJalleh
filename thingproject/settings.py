9dc4e575b5bd27a1215fcf530f211e9276228611
Brian Jalleh <brjalleh@gmail.com>
2022-10-14 18:47:41 +0100

    add

--- thingproject/settings.py
+++ thingproject/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'things'
+    'Things'
 ]
 
 MIDDLEWARE = [
@@ -127,4 +127,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 ## User model for authentication purposes
-AUTH_USER_MODEL = 'things.Thing'
+AUTH_USER_MODEL = 'Things.Thing'
