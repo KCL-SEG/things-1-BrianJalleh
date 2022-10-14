d7d6eaad63ee759b8772988cfbdca382df628800
Brian Jalleh <brjalleh@gmail.com>
2022-10-14 19:15:37 +0100

    Fix contraints

--- Things/models.py
+++ Things/models.py
@@ -4,8 +4,8 @@ from django.core import validators
 # Create your models here.
 class Thing(models.Model):
     name = models.SlugField(unique=True,validators=[validators.MinLengthValidator(1), validators.MaxLengthValidator(30)])
-    description = models.TextField(blank=True, validators=[validators.MaxLengthValidator(120)])
-    quantity = models.IntegerField(validators=[validators.MinValueValidator(0), validators.MaxValueValidator(100)])
+    description = models.TextField(unique = False, blank=True, validators=[validators.MaxLengthValidator(120)])
+    quantity = models.IntegerField(unique = False, validators=[validators.MinValueValidator(0), validators.MaxValueValidator(100)])
 
     # name = models.SlugField(
     #     unique = True, max_length = 30, blank = False
